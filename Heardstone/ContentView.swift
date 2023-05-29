1457e80fe63efd05ebdf3ace40858db8dfa6185c
Gerson Arbigaus <gerson87@gmail.com>
2023-05-29 20:24:03 -0300

    Initial Commit

---
+++ Heardstone/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Heardstone
+//
+//  Created by Gerson Arbigaus on 29/05/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Heardstone/HeardstoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeardstoneApp.swift
+//  Heardstone
+//
+//  Created by Gerson Arbigaus on 29/05/23.
+//
+
+import SwiftUI
+
+@main
+struct HeardstoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
