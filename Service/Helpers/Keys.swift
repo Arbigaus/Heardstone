68f5053d22abe9e9c2efcdeb192ecfc1a9436bd5
Gerson Arbigaus <gerson87@gmail.com>
2023-05-30 19:41:19 -0300

    Add the Kyes enum to save the API keys

---
+++ Service/Helpers/Keys.swift
@@ -0,0 +1,13 @@
+//
+//  Keys.swift
+//  Heardstone
+//
+//  Created by Gerson Arbigaus on 30/05/23.
+//
+
+import Foundation
+
+enum Keys: String {
+    case privateKey = "Some Key"
+    case privateHost = "Some private host"
+}
